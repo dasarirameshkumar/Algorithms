6fb5631d947ec03292c9194057dc26417bb54df0
rd293h <rd293h@att.com>
2017-02-24 10:06:50 -0500

    Linear Search

---
+++ LinearSearch/LinearSearch/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearSearch
+{        
+    class Program
+    {
+        static int linearSearch(int[] eleArray, int eleToSearch)
+        {
+            int iResult = -1;
+            for (int i = 0; i < eleArray.Length; i++)
+            {
+                if (eleArray[i] == eleToSearch)
+                {
+                    iResult = i;
+                    break;
+                }
+            }
+            return iResult;
+        }
+
+        static void Main(string[] args)
+        {
+                int[] elements = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+                int position = linearSearch(elements, 1);
+                if (position == -1)
+                    Console.WriteLine("Element is not found");
+                else
+                    Console.WriteLine("Element is found at position: " + position);
+        }
+    }
+}
